63b33102a2c719dde72c2026b3b069d4d9096d5e
eswar.praphulv <eswar.praphulv@nutanix.com>
2019-05-23 10:09:54 -0700

    first commit

---
+++ terminal/urls.py
@@ -0,0 +1,11 @@
+
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^cmd-json/', views.event_cmd_json, name='event_cmd_json'),
+    url(r'^wed-json/', views.event_wed_json, name='event_wed_json'),
+    url(r'^init-json/', views.event_init_json, name='event_init_json'),
+    url(r'^', views.index, name='index'),
+]
